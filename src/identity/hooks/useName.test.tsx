083e7ef328c7fd49d3de0d022bb64baa2209e3b6
Léo Galley <contact@kirkas.ch>
2024-07-11 15:28:08 -0400

    add chain to useName, prefer Promise.reject over throw new Error

--- src/identity/hooks/useName.test.tsx
+++ src/identity/hooks/useName.test.tsx
@@ -6,6 +6,7 @@ import { renderHook, waitFor } from '@testing-library/react';
 import { useName } from './useName';
 import { publicClient } from '../../network/client';
 import { getNewReactQueryTestProvider } from './getNewReactQueryTestProvider';
+import { base, optimism } from 'viem/chains';
 
 jest.mock('../../network/client');
 jest.mock('../../network/getChainPublicClient', () => ({
@@ -15,13 +16,14 @@ jest.mock('../../network/getChainPublicClient', () => ({
 
 describe('useName', () => {
   const mockGetEnsName = publicClient.getEnsName as jest.Mock;
+  const mockReadContract = publicClient.readContract as jest.Mock;
+  const testAddress = '0x123';
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('returns the correct ENS name and loading state', async () => {
-    const testAddress = '0x123';
     const testEnsName = 'test.ens';
 
     // Mock the getEnsName method of the publicClient
@@ -41,8 +43,6 @@ describe('useName', () => {
   });
 
   it('returns the loading state true while still fetching from ens action', async () => {
-    const testAddress = '0x123';
-
     // Use the renderHook function to create a test harness for the useName hook
     const { result } = renderHook(() => useName({ address: testAddress }), {
       wrapper: getNewReactQueryTestProvider(),
@@ -53,6 +53,44 @@ describe('useName', () => {
       // Check that the ENS name and loading state are correct
       expect(result.current.data).toBe(undefined);
       expect(result.current.isLoading).toBe(true);
+    });
+  });
+
+  it('returns the correct ENS name and loading state for custom chain ', async () => {
+    const testEnsName = 'test.customchain.eth';
+
+    // Mock the getEnsName method of the publicClient
+    mockReadContract.mockResolvedValue(testEnsName);
+
+    // Use the renderHook function to create a test harness for the useName hook
+    const { result } = renderHook(
+      () => useName({ address: testAddress, chain: base }),
+      {
+        wrapper: getNewReactQueryTestProvider(),
+      },
+    );
+
+    // Wait for the hook to finish fetching the ENS name
+    await waitFor(() => {
+      // Check that the ENS name and loading state are correct
+      expect(result.current.data).toBe(testEnsName);
+      expect(result.current.isLoading).toBe(false);
+    });
+  });
+
+  it('returns undefined unsupported chain ', async () => {
+    // Use the renderHook function to create a test harness for the useName hook
+    const { result } = renderHook(
+      () => useName({ address: testAddress, chain: optimism }),
+      {
+        wrapper: getNewReactQueryTestProvider(),
+      },
+    );
+
+    // Wait for the hook to finish fetching the ENS name
+    await waitFor(() => {
+      // Check that the ENS name and loading state are correct
+      expect(result.current.data).toBe(undefined);
     });
   });
 });
